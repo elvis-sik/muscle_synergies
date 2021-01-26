5b59246d87fee885b26e6df36d25d8a721453663
Elvis Sikora <elvis.sikora@gmail.com>
2021-01-26 15:05:41 -0300

    Added detailed specs + a couple of tests for AllDevicesDataBuilder

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -63,7 +63,141 @@ class TestDeviceHeaderDataBuilder:
 
 
 class TestAllDevicesDataBuilder:
-    pass
+    """Class spec:
+
+    1. initialization:
+
+    Arguments:
+    - force_plates (Mapping[str, List[DeviceHeaderCols]]): a dict like
+      {'Force plate 1': [its, DeviceHeaderCols]}
+    - emg_device (DeviceHeaderCols)
+    - trajectory_marker_devices (List[DeviceHeaderCols])
+    - device_header_data_builder_type (optional): the class used to represent
+      individual device headers. By default, it is DeviceHeaderDataBuilder
+    - time_series_data_builder_type (optional): the class used to represent
+      individual time series. By default, it is TimeSeriesDataBuilder.
+
+    Behavior:
+    - initializes one DeviceHeaderDataBuilder per DeviceHeaderCols it is
+      fed
+    - the class blindly believes the data it is provided with makes sense
+    """
+    @pt.fixture
+    def mock_device_header_data_builder_type(self, mocker):
+        return mocker.create_autospec(vd.DeviceHeaderDataBuilder)
+
+    @pt.fixture
+    def mock_time_series_data_builder_type(self, mocker):
+        return mocker.create_autospec(vd.TimeSeriesDataBuilder)
+
+    @pt.fixture
+    def force_plate_header_cols(self):
+        return {
+            'first force plate': [
+                vd.DeviceHeaderCols(device_name='first force plate',
+                                    device_type=vd.DeviceType.FORCE_PLATE,
+                                    first_col_index=2)
+            ]
+        }
+
+    @pt.fixture
+    def trajectory_marker_header_cols(self):
+        return vd.DeviceHeaderCols(device_name='trajectory marker',
+                                   device_type=vd.DeviceType.TRAJECTORY_MARKER,
+                                   first_col_index=5)
+
+    @pt.fixture
+    def emg_header_cols(self):
+        return vd.DeviceHeaderCols(device_name='emg',
+                                   device_type=vd.DeviceType.EMG,
+                                   first_col_index=8)
+
+    @pt.fixture
+    def all_devices_data_builder(self, mock_device_header_data_builder_type,
+                                 mock_time_series_data_builder_type,
+                                 force_plate_header_cols,
+                                 trajectory_marker_header_cols,
+                                 emg_header_cols):
+        return vd.AllDevicesDataBuilder(
+            force_plates=force_plater_header_cols,
+            emg_device=emg_header_cols,
+            trajectory_marker_devices=trajectory_marker_header_cols,
+            device_header_data_builder_type=
+            mock_device_header_data_builder_type,
+            time_series_data_builder_type=mock_time_series_data_builder_type,
+        )
+
+    def test_initializes_data_header_for_force_plate(
+            self, all_devices_data_builder,
+            mock_device_header_data_builder_type,
+            mock_time_series_data_builder_type):
+        device_name = 'first force plate'
+        device_type = vd.DeviceType.FORCE_PLATE
+        mock_time_series_data_builder_type
+        mock_device_header_data_builder_type.__init__.assert_called_with(
+            device_name=device_name,
+            device_type=device_type,
+            time_series_data_builder_type=mock_time_series_data_builder_type)
+
+    def test_initializes_data_header_for_trajectory_marker(
+            self, all_devices_data_builder,
+            mock_device_header_data_builder_type,
+            mock_time_series_data_builder_type):
+        device_name = 'trajectory marker'
+        device_type = vd.DeviceType.TRAJECTORY_MARKER
+        mock_time_series_data_builder_type
+        mock_device_header_data_builder_type.__init__.assert_called_with(
+            device_name=device_name,
+            device_type=device_type,
+            time_series_data_builder_type=mock_time_series_data_builder_type)
+
+    def test_initializes_data_header_for_force_plate(
+            self, all_devices_data_builder,
+            mock_device_header_data_builder_type,
+            mock_time_series_data_builder_type):
+        device_name = 'emg'
+        device_type = vd.DeviceType.EMG
+        mock_time_series_data_builder_type
+        mock_device_header_data_builder_type.__init__.assert_called_with(
+            device_name=device_name,
+            device_type=device_type,
+            time_series_data_builder_type=mock_time_series_data_builder_type)
+
+    # 2. has a add_coordinates method:
+    #
+    #    Arguments:
+    #    - parsed_row (Row): the coordinates line of the input with its empty
+    #                        columns at the end stripped.
+    #
+    #    Behaviors:
+    #    - passes downstream the columns for the DeviceHeaderDataBuilder
+    #      instances that were created during AllDevicesDataBuilder
+    #      initialization.
+    #    - the method it calls is named add_coordinates as well
+    #    - each device spans 3 columns, the first one being the one given in
+    #      AllDevicesDataBuilder initialization
+    #    - EMG devices are different, they have a variable number of columns
+    #      which should corresponds to the number of columns from its first
+    #      until the final one provided
+
+    # 3. has a add_units method
+    #
+    #    Arguments:
+    #    - physical_units_row (List[pint.Unit]): the units line of the csv file
+    #                                            with its empty columns at the
+    #                                            end stripped
+    #
+    #    Behaviors:
+    #    - similar to add_coordinates
+
+    # 4. has a add_data method
+    #
+    #    Arguments:
+    #    - data_row (List[float]): a data line of the csv file with its empty
+    #                              columns at the end stripped
+    #
+    #    Behaviors:
+    #    - similar to add_coordinates
 
 
 class TestSectionDataBuilder:

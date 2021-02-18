07591d2718d72e10e9ab28aaacdec9d9c1e3096d
Elvis Sikora <elvis.sikora@gmail.com>
2021-02-18 16:12:22 -0300

    Move ForcePlateGrouper to be before _DevicesState ABC
    Seemed to make more sense. Previous reasoning was Grouper was only used by
    ForcesEMGDevicesState. OTOH, I prefer having the parent class close to its
    childs.

--- src/muscle_synergies/vicon_data/reader.py
+++ src/muscle_synergies/vicon_data/reader.py
@@ -288,6 +288,39 @@ class DevicesHeaderFinder:
         return (col_num - 2) % 3 == 0
 
 
+class ForcePlateGrouper:
+    def group(self, headers: List[ColOfHeader]) -> List[ColOfHeader]:
+        grouped_headers = []
+        for header in self._filter_individual_force_plates(headers):
+            grouped_headers.append(self._rename_force_plate(header))
+        return grouped_headers
+
+    def _filter_individual_force_plates(self, headers: List[ColOfHeader]
+                                        ) -> Iterator[ColOfHeader]:
+        for i, head in enumerate(headers):
+            if i % 3 == 0:
+                yield head
+
+    def _rename_force_plate(self, header: ColOfHeader) -> ColOfHeader:
+        header_str = self._header_str(header)
+        new_name = self._get_force_plate_name(header_str)
+        first_col = self._col_of_header_first_col(header)
+        return self._col_of_header(new_name, first_col)
+
+    def _get_force_plate_name(self, header_str: str):
+        force_plate_name, measurement_name = header_str.split('-')
+        return force_plate_name[:-1]
+
+    def _col_of_header_header_str(self, header: ColOfHeader) -> str:
+        return header.header_str
+
+    def _col_of_header_first_col(self, header: ColOfHeader) -> int:
+        return header.col_index
+
+    def _col_of_header(self, header_str: str, first_col: int) -> ColOfHeader:
+        return ColOfHeader(header_str, first_col)
+
+
 class _DevicesState(_UpdateStateMixin, _ReaderState):
     finder: DevicesHeaderFinder
 
@@ -352,39 +385,6 @@ class _DevicesState(_UpdateStateMixin, _ReaderState):
     @abc.abstractmethod
     def _last_col(self, device_type: DeviceType, first_col: int) -> Any:
         pass
-
-
-class ForcePlateGrouper:
-    def group(self, headers: List[ColOfHeader]) -> List[ColOfHeader]:
-        grouped_headers = []
-        for header in self._filter_individual_force_plates(headers):
-            grouped_headers.append(self._rename_force_plate(header))
-        return grouped_headers
-
-    def _filter_individual_force_plates(self, headers: List[ColOfHeader]
-                                        ) -> Iterator[ColOfHeader]:
-        for i, head in enumerate(headers):
-            if i % 3 == 0:
-                yield head
-
-    def _rename_force_plate(self, header: ColOfHeader) -> ColOfHeader:
-        header_str = self._header_str(header)
-        new_name = self._get_force_plate_name(header_str)
-        first_col = self._col_of_header_first_col(header)
-        return self._col_of_header(new_name, first_col)
-
-    def _get_force_plate_name(self, header_str: str):
-        force_plate_name, measurement_name = header_str.split('-')
-        return force_plate_name[:-1]
-
-    def _col_of_header_header_str(self, header: ColOfHeader) -> str:
-        return header.header_str
-
-    def _col_of_header_first_col(self, header: ColOfHeader) -> int:
-        return header.col_index
-
-    def _col_of_header(self, header_str: str, first_col: int) -> ColOfHeader:
-        return ColOfHeader(header_str, first_col)
 
 
 class ForcesEMGDevicesState(_DevicesState):

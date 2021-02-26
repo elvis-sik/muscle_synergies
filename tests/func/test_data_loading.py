ad4520dd8717146a3abf0beb9986806ec906b6ab
Elvis Sikora <elvis.sikora@gmail.com>
2021-02-26 11:41:42 -0300

    Make fixture parametrized instead of test

--- tests/func/test_data_loading.py
+++ tests/func/test_data_loading.py
@@ -10,9 +10,13 @@ sample_data_dir = project_root / 'sample_data'
 abridged_csv = sample_data_dir / 'abridged_data.csv'
 
 
-@pt.mark.parametrize('csv_file', [abridged_csv])
-def test_loads_data(csv_file):
-    vicon_nexus_data = vd.load_vicon_file(csv_file)
+@pt.fixture(params=(abridged_csv, ), scope='module')
+def vicon_nexus_data(request):
+    csv_file = request.param
+    return vd.load_vicon_file(csv_file)
+
+
+def test_loads_data(vicon_nexus_data):
     assert vicon_nexus_data.emg is not None
     assert vicon_nexus_data.force_plates is not None
     assert vicon_nexus_data.trajectory_markers is not None

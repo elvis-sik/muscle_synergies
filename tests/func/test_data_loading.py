3794a230a086be77aa34263a2f368540b588aab0
Elvis Sikora <elvis.sikora@gmail.com>
2021-02-28 00:32:33 -0300

    Make test_loads_device_data_correctly parametric

--- tests/func/test_data_loading.py
+++ tests/func/test_data_loading.py
@@ -1,20 +1,26 @@
 import pytest as pt
 
-
-def test_loads_data(abridged_data):
-    assert abridged_data.emg is not None
-    assert abridged_data.force_plates is not None
-    assert abridged_data.trajectory_markers is not None
-
-
-@pt.mark.parametrize('loaded_dev_data,exp_dev_data', [
+emg_loaded_exp = [
     pt.lazy_fixture(['loaded_emg', 'exp_emg']),
+]
+
+fp_loaded_exp = [
     pt.lazy_fixture(['loaded_forcep1', 'exp_forcep1']),
+    pt.lazy_fixture(['loaded_angelica_hv', 'exp_angelica_hv'])
+]
+
+forces_emg_loaded_exp = emg_loaded_exp + fp_loaded_exp
+
+traj_loaded_exp = [
     pt.lazy_fixture(['loaded_forcep2', 'exp_forcep2']),
-    pt.lazy_fixture(['loaded_angelica_hv', 'exp_angelica_hv']),
     pt.lazy_fixture(['loaded_angelica_cme', 'exp_angelica_cme']),
     pt.lazy_fixture(['loaded_angelica_cle', 'exp_angelica_cle']),
     pt.lazy_fixture(['loaded_angelica_elastdp', 'exp_angelica_elastdp']),
-])
-def test_loads_device_data_correctly(loaded_dev_data, exp_dev_data):
+]
+
+all_loaded_exp = forces_emg_loaded_exp + traj_loaded_exp
+
+
+@pt.mark.parametrize('loaded_dev_data,exp_dev_data', all_loaded_exp)
+def test_loads_device_data_correct(loaded_dev_data, exp_dev_data):
     assert loaded_dev_data == exp_dev_data

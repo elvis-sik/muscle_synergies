Fix formula for VAF
So if I understood this correctly (Rabbi et al, 2020) had an error in
their formula. I used the version from (Saito et al, 2018) which I also
saw in other papers.

---
Saito et al, 2018
https://www.nature.com/articles/s41598-018-24332-z#Sec2

src/muscle_synergies/analysis.py
@@ -135,8 +135,8 @@ def nnmf(matrix_df, num_components, *, max_iter=100_000, tol=1e-6):
 
 
 def vaf(original_df, transformed_df, components):
-    square_error = (original_df - transformed_df @ components) ** 2
-    return 1 - np.linalg.norm(square_error) / np.linalg.norm(original_df)**2
+    error = original_df - transformed_df @ components
+    return 1 - np.linalg.norm(error, ord=2) / np.linalg.norm(original_df, ord=2)
 
 
 def find_synergies(